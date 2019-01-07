3ac2f143e58fa0b447f5273e6be54c9baeefac09
chunbong <cdy0266@gmail.com>
2019-01-07 23:48:53 +0900

    Updates

---
+++ precache-manifest.7c9fbb19b4c952c27bb4f501c2d3abc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "7ec53335b26f0c3317e8",
+    "url": "/movie_app/static/js/main.7ec53335.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "7ec53335b26f0c3317e8",
+    "url": "/movie_app/static/css/main.4988837b.chunk.css"
+  },
+  {
+    "revision": "2384d04f21100894f51d4bebafe8a3a9",
+    "url": "/movie_app/index.html"
+  }
+];
