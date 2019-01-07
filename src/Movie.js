df9b33e1954505f13f566a4f4813581196cb980e
chunbong <cdy0266@gmail.com>
2019-01-07 23:32:42 +0900

    Modify Movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
-import LinesEllipsis from 'react-lines-ellipsis';
 
 function Movie({title, poster, genres, synopsis}){
-  return(
+  return (
     <div className="Movie">
-      <div className="Movie_Columns">
+      <div className="Movie__Column">
         <MoviePoster poster={poster} alt={title} />
-      </div>
-      <div className="Movie_Columns">
+      </div>   
+      <div className="Movie__Column">
         <h1>{title}</h1>
-        <div className="Movie_Genres">
+        <div className="Movie__Genres">
           {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
         </div>
-        <div className="Movie_synopsis">
+        <div className="Movie__Synopsis">
         <LinesEllipsis
           text={synopsis}
           maxLine='3'
           ellipsis='...'
           trimRight
           basedOn='letters'
-        />
+          />   
         </div>
       </div>
     </div>
@@ -29,31 +29,31 @@ function Movie({title, poster, genres, synopsis}){
 }
 
 function MoviePoster({poster, alt}){
-  return(
-    <img src={poster} alt={alt} title={alt} className="Movie_Poster" />
+  return (
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
   )
 }
 
 function MovieGenre({genre}){
-  return(
-    <span className="Movie_Genres">{genre}</span>
+  return (
+    <span className="Movie__Genre">{genre}</span>
   )
 }
 
-Movie.PropTypes = {
-  title : PropTypes.string.isRequired,
-  poster : PropTypes.string.isRequired,
-  genres : PropTypes.array.isRequired,
-  synopsis : PropTypes.string.isRequired
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
 }
 
-MoviePoster.PropTypes = {
-  poster : PropTypes.string.isRequired,
-  alt : PropTypes.string.isRequired
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
 }
 
-MovieGenre.PropTypes = {
-  genre : PropTypes.string.isRequired
+MovieGenre.propTypes ={
+  genre: PropTypes.string.isRequired
 }
 
-export default Movie;
+export default Movie
