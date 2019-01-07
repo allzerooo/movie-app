6af971e86efed5d57ca763b1f11bd9857ae5ab92
chunbong <cdy0266@gmail.com>
2019-01-07 21:44:31 +0900

    Add Stateless Functional Components, Promise

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-  render() {
-    return(
-        <h1>hello</h1>        
-    )
-  }
+function Movie({title, poster, genres, synopsis}){
+  return(
+    <div className="Movie">
+      <div className="Movie_Columns">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className="Movie_Columns">
+        <h1>{title}</h1>
+        <div className="Movie_Genres">
+          {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+        </div>
+        <p className="Movie_synopsis">
+          {synopsis}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+function MoviePoster({poster, alt}){
+  return(
+    <img src={poster} alt={alt} title={alt} className="Movie_Poster" />
+  )
+}
+
+function MovieGenre({genre}){
+  return(
+    <span className="Movie_Genres">{genre}</span>
+  )
+}
+
+Movie.PropTypes = {
+  title : PropTypes.string.isRequired,
+  poster : PropTypes.string.isRequired,
+  genres : PropTypes.array.isRequired,
+  synopsis : PropTypes.string.isRequired
+}
+
+MoviePoster.PropTypes = {
+  poster : PropTypes.string.isRequired,
+  alt : PropTypes.string.isRequired
+}
+
+MovieGenre.PropTypes = {
+  genre : PropTypes.string.isRequired
 }
 
 export default Movie;
